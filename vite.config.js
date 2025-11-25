0ade5bda9f130177368a24acae5c3e77a8ee7bf8
virunpatch <guyver125@hotmail.com>
2025-11-25 18:46:28 +0700

    Config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// แทนที่ 'Vue.js-ToDoList' ด้วยชื่อ Repository ของคุณ
+const repoName = 'Vue.js-ToDoList'; 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/', // <--- เพิ่มบรรทัดนี้
   plugins: [
     vue(),
     vueDevTools(),
